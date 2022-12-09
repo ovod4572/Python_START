28d2e8ccf7bd62f101b57a55a95c60c5a3058639
ovod4572 <intorgsv@gmail.com>
2022-12-09 16:22:39 +0300

    Task_4 solved

--- HW_1/task_4.py
+++ HW_1/task_4.py
@@ -10,3 +10,15 @@
 3
 x < 0, y < 0
 """
+a = int(input('Enter quarter number: '))
+
+if a == 1:
+    print('quarter = 1 => x∈(0, ∞) и y∈(0, ∞)')
+elif a == 2:
+    print('quarter = 2 => x∈(0, -∞) и y∈(0, ∞)')
+elif a == 3:
+    print('quarter = 3 => x∈(0, -∞) и y∈(0, -∞)')
+elif a == 4:
+    print('quarter = 4 => x∈(0, ∞) и y∈(0, -∞)')
+else:
+    print('Input Error')
