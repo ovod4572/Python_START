c39aeee994a69914c8ef12ce89996c05b7141e59
ovod4572 <intorgsv@gmail.com>
2022-12-10 18:22:33 +0300

    Task_4 solved

--- HW_2/task_4.py
+++ HW_2/task_4.py
@@ -6,3 +6,17 @@
 Ввод: значение типа <int>
 Вывод: значение типа <int>
 """
+
+from random import randint
+
+num = int(input('Enter the number: '))
+new_list = [randint(-num, num) for r in range(num)]
+index_list = []
+prod = 1
+data = open('indexes.txt', 'r')
+for index in data:
+    index_list.append(int(index))
+for i in range(-num, num):
+    if i in index_list:
+        prod *= new_list[i]
+print(prod)
