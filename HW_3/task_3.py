ba87a6037a142fb2c36887e13056691daf76bb84
ovod4572 <intorgsv@gmail.com>
2022-12-11 14:35:11 +0300

    Task_3 solved

--- HW_3/task_3.py
+++ HW_3/task_3.py
@@ -9,3 +9,18 @@
 [1.1, 1.2, 3.1, 5, 10.01]
 2.0
 """
+from random import uniform
+
+len_array = int(input("Введите количество чисел в списке: "))
+sign = int(input("Введите количество знаков после запятой: "))
+float_numbers = []
+
+for i in range(len_array):
+    float_numbers.append(round(uniform(1, 10), sign))   
+print(float_numbers)
+
+for i in range(len_array):
+    float_numbers[i] = float_numbers[i] - int(float_numbers[i])
+float_numbers.sort()
+dif_numbers = float_numbers[-1] - float_numbers[0]
+print(f' => {round(dif_numbers, sign)}')
