22a2e97e1d0f70cbffa0c4a73176bb4a2a685ef1
ovod4572 <intorgsv@gmail.com>
2022-12-11 15:48:29 +0300

    Task_4 solved

--- HW_3/task_4.py
+++ HW_3/task_4.py
@@ -14,3 +14,15 @@
 2
 10
 """
+number = int(input("Enter the number: "))
+def binary(number):
+    if number == 0:
+        return 0
+    
+    remains = binary(number // 2)
+    number = number % 2 + 10 * remains
+#    print(number)
+    return number
+    
+print(binary(number))
+
