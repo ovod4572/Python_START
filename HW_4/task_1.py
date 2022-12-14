91d08f26b58be73f8dc03d36f4b383a25c478198
ovod4572 <intorgsv@gmail.com>
2022-12-14 12:45:09 +0300

    Task_1 solved

--- HW_4/task_1.py
+++ HW_4/task_1.py
@@ -12,3 +12,13 @@
 [2, 19]
 """
 
+number = int(input("Enter the number: "))
+i = 2
+simpie_multiplier = []
+while i <= number:
+    if number % i == 0:
+        simpie_multiplier.append(i)
+        number //= i
+    else:
+        i += 1
+print(simpie_multiplier)
