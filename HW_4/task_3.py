7df5f839ab3c6bb1756e2ae99908d67b533c5e5a
ovod4572 <intorgsv@gmail.com>
2022-12-15 18:40:59 +0300

    Task_3 solved

--- HW_4/task_3.py
+++ HW_4/task_3.py
@@ -10,3 +10,25 @@
 2
 2x^2 + 4x + 5 = 0
 """
+
+from random import randint
+
+k = int(input('Введите натуральную степень k: '))
+ratios = [randint(0, 100) for i in range(k)] + [randint(1, 100)]
+power = len(ratios) - 1
+polinom = []
+
+for ratio in ratios:
+    if ratio:
+        if power == 0:
+            polinom. append(f'{ratio}')
+        elif power == 1:
+            polinom.append(f'{ratio if ratio != 1 else ""}x')
+        else:
+            polinom.append(f'{ratio if ratio != 1 else ""}x^{power}')
+    power -= 1
+polinom = ' + '.join(polinom) + ' = 0'
+print(polinom)
+with open('task_4.txt', 'w') as data:
+    data.write(polinom)
+    
