a04f9f35dc6858a7ebbba249c8240c46eb9a5a98
ovod4572 <intorgsv@gmail.com>
2022-12-17 12:24:34 +0300

    Task_1 solved

--- HW_5/task_1.py
+++ HW_5/task_1.py
@@ -4,3 +4,31 @@
 Ввод: значение типа <str>
 Вывод: значение типа <str>
 """
+
+#arg = input("Enter text separated by a space:\n")
+arg = "автобус генерал швабра итог арбуз вагон"
+print(arg)
+
+
+def new_text(arg):
+    arg = list(filter(lambda x: "а" and "б" and "в" not in x, arg.split()))
+    return " ".join(arg)
+
+result = new_text(arg)
+print(result)
+
+
+# new_text = [i for i in arg.split() if not ("а" in i and "б" in i and "в" in i)]
+# print(" ".join(new_text))
+
+
+# text = arg.split()
+# new_text = []
+
+# for i in text:
+#     if not ("а" in i and "б" in i and "в" in i):
+#         new_text.append(i)
+
+# result = ' '.join(new_text) 
+# print(result)
+
