525fa0c1b9fbc780ab461805b4094c45e0635bc4
ovod4572 <intorgsv@gmail.com>
2022-12-24 19:58:07 +0300

    Task_4 solved

--- HW_5/task_4.py
+++ HW_5/task_4.py
@@ -10,3 +10,27 @@
 ыыыыыррррр   аааааагггггггг
 5ы5р3 6а8г
 """
+#enc = input()
+enc = 'ыыыыыррррр   аааааагггггггг'
+conut = 1
+for i in range(len(enc)):
+    if i == (len(enc) - 1):
+        print(str(conut) + enc[i], end='')
+    else:
+        if enc[i] == enc[i + 1]:
+            conut = conut + 1
+        else:
+            print(str(conut) + enc[i], end='')
+            conut = 1
+print()
+
+#dec = input()
+dec = '5ы5р3 6а8г'
+for i in range(len(dec)):
+    if dec[i].isdigit():
+        for j in range(int(dec[i]) - 1):
+            print(dec[i + 1], end='')
+    else:
+        print(dec[i], end='')
+print()
+
