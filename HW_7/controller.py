f70366b360482862c67d6e4c0deddc8bc41f34b4
ovod4572 <intorgsv@gmail.com>
2022-12-30 17:47:42 +0300

    Controller solved

--- HW_7/controller.py
+++ HW_7/controller.py
@@ -3,6 +3,27 @@ import view
 from logger import log
 
 
+@log
 def start():
+    """Стартовая функция"""
+    view.greatings()
+    button = view.menu()
 
-    pass
+    if button == 1:
+        view.print_book(model.get_data())
+        start()
+    elif button == 2:
+        model.add_data(view.add_record())
+        start()
+    elif button == 3:
+        model.add_data(view.editor(model.get_data_id(view.request_id())))
+        start()
+    elif button == 4:
+        view.print_book(model.get_data_last_name(view.request_last_name()))
+        start()
+    else:
+        print('Работа окончена')
+        exit
+        print()
+
+        
